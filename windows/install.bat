f3fc24b48bfb41ee2eaba0606a72038d8af4648a
Arvioss <arvioss@users.noreply.github.com>
2026-09-01 17:28:45 +0000

    Add 1-click .bat launchers for Windows ease of use

---
+++ windows/install.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ====================================================
+echo      Oracle Cloud 24/7 VPN 1-Click Installer
+echo ====================================================
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0install-windows.ps1"
+pause
---
+++ windows/start.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0oracle-vpn.ps1" start
+pause
